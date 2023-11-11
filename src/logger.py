acbef1e3aea3d3767b1d22a36be09701932748be
Susmitha <susmithakamu1@gmail.com>
2023-11-11 12:23:23 +0530

    Model trained

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from datetime import  datetime
+from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
@@ -13,3 +13,6 @@ logging.basicConfig(
     format = "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level= logging.INFO,
 )
+
+if __name__ == "__main__":
+    logging.info("Logging has started")
