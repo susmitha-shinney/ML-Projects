ba1ec7afe4ca79eb0ec2596ba9c999f9e8794fda
Susmitha <susmithakamu1@gmail.com>
2023-12-04 22:51:26 +0530

    deployment using AWS elasticbeanstalk

--- src/Components/data_transformation.py
+++ src/Components/data_transformation.py
@@ -83,11 +83,11 @@ class DataTransformation:
                 "lunch",
                 "test_preparation_course"]'''
 
-            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
-            target_feature_train_df=train_df[target_column_name]
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1) #X_train
+            target_feature_train_df=train_df[target_column_name] #Y_train
 
-            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
-            target_feature_test_df=test_df[target_column_name]
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1) #X_test
+            target_feature_test_df=test_df[target_column_name] #Y_test
 
             logging.info(
                 f"Applying preprocessing object on training dataframe and testing dataframe."
--- src/Components/model_trainer.py
+++ src/Components/model_trainer.py
@@ -48,18 +48,50 @@ class ModelTrainer:
                 "AdaBoost Regression": AdaBoostRegressor(),
             }
 
-            model_report: dict = evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params = {
+                "Decision Tree": {
+                      'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                                       
+                },
+                "Random Forest":{
+                     'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting": {
+                     'learning_rate':[.1,.01,.05,.001],
+                     'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                     'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbour Regressor":{
+                     'n_neighbors':[5,7,9,11],
+                },
+                "XGBRegressor":{
+                     'learning_rate':[.1,.01,.05,.001],
+                     'n_estimators':[8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                     'depth':[6,8,10],
+                     'learning_rate':[0.01,0.05,0.1],
+                     'iterations':[30,50,100]
+                },
+                "AdaBoost Regressor":{
+                     'learning_rate':[.1,.01,0.5,.001],
+                     'n_estimators':[8,16,32,64,128,256]
+                }
+            }
 
+            model_report: dict = evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,params=params)
+            #print(model_report)
             # To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
-            
+            #print(best_model_score)
             # To get best model name from dict
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-
+            #print(best_model_name)
             best_model = models[best_model_name]
-
+            #print(best_model)
             if best_model_score<0.6:
                 raise CustomException("No best model found")
             
